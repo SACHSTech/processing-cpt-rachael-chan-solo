25ee8ab3d9d0dbb74e8811f8c5fd05aac04532ca
rachaelhychan <rachael.chan25@ycdsbk12.ca>
2024-01-14 03:40:43 -0500

    work 1

--- Sketch.java
+++ Sketch.java
@@ -1,36 +1,225 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Sketch extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
+    PImage imgBackground;
+    PImage imgBasket;
+    PImage[] imgBurgerImages = new PImage[5]; //Array will hold four images
+    Float[] fltImageX = new Float [5]; // hold X positions of images
+    Float[] fltImageY = new Float[5]; // hold y opsitions of the images
+    Boolean[] blnImageHide = {false, false, false, false, false};  
+    Float[] fltImageSpeed = {(float)random(3,4), (float)random(3,4),
+                        (float)random(3,4), (float)random(3,4),
+                        (float)random(3, 4)};
+    
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    ArrayList<Integer> order = new ArrayList<>();  // ArrayList to hold the correct order of the images
+    ArrayList<Integer> caught = new ArrayList<>(); // ArrayList to hold the images caught by the catcher
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
   
-  // define other methods down here.
+    int intCatcherX;
+    int intTime1 = 1000;
+
+
+    public void settings() {
+        // size call
+        size(800, 600);
+    }
+
+    public void setup() {
+        imgBackground = loadImage("Kitchen.png");
+        imgBackground.resize(800, 600);
+        imgBasket = loadImage("data/basket.png");
+        imgBasket.resize(150, 75);
+        
+
+        // Set the initial position of the catcher
+        intCatcherX = width / 2;
+
+        imgBurgerImages[0] = loadImage("data/burger bun.png");
+        imgBurgerImages[0].resize(75, 75);
+        imgBurgerImages[1] = loadImage("data/burger patty.png");
+        imgBurgerImages[1].resize(75,75);
+        imgBurgerImages[2] = loadImage("data/burger cheese.png");
+        imgBurgerImages[2].resize(75, 75);
+        imgBurgerImages[3] = loadImage("data/burger lettuce.png");
+        imgBurgerImages[3].resize(75, 75);
+        imgBurgerImages[4] = loadImage("data/burger sauce.png");
+        imgBurgerImages[4].resize(75, 75);
+
+        Arrays.setAll(fltImageX, i -> (float)random(width));
+        Arrays.setAll(fltImageY, i -> (float)(imgBurgerImages[i].height) * -1);
+
+        order.addAll(Arrays.asList(0, 1, 2, 3, 4));
+    
+        Collections.shuffle(Arrays.asList(fltImageX));
+        Collections.shuffle(Arrays.asList(fltImageY));
+        Collections.shuffle(order);
+ 
+    }
+
+    
+
+    /**
+     * Called repeatedly, anything drawn to the screen goes here
+     */
+    public void draw() {
+        catchIngredients();
+    }
+
+    public int catchIngredients(){
+
+        intTime1 += -1;
+
+        // draw background
+        image(imgBackground, 0, 0);
+
+
+        for(int i = 0; i < imgBurgerImages.length; i++) {
+
+            if (blnImageHide[i] == false){
+                image(imgBurgerImages[i], fltImageX[i], fltImageY[i]);
+                fltImageY[i] += fltImageSpeed[i]; 
+            }
+
+            if (fltImageY[i] < 20){
+                break;
+            }
+
+            if(fltImageY[i] > 360 && fltImageY[i] < 400){
+                if (abs(fltImageX[i] - (intCatcherX+40)) <= 75){
+                    blnImageHide[i] = true;
+                }
+            }
+
+            if (fltImageY[i] > height) {
+                fltImageY[i] = (float) 21;
+                fltImageX[i] = random(width);
+                fltImageSpeed[i] = random(3,4);
+            }
+
+            if(blnImageHide[0] == true && blnImageHide[1] == true && blnImageHide[2] == true && blnImageHide[3] == true && blnImageHide[4] == true){
+                return 1;
+            }
+
+            if(intTime1 <= 0){
+                for (i = 0; i < imgBurgerImages.length; i++){
+                    blnImageHide[i] = true;
+                }
+                return 2;
+            }
+        }
+
+        // Draw the catcher 
+        image(imgBasket, intCatcherX, 380);
+        timer1();
+
+        return 2;
+
+    }
+
+    public void timer1(){
+        fill(255, 255, 255);
+        rect(300, 500, 200, 40);
+            
+        fill(0, 0, 0);
+        text("Time Left: ",315, 530);
+            textSize(25);
+        
+        if(intTime1 > 900){
+            fill(255, 0, 0);
+            text("10",443, 530);
+            textSize(25);
+        }else if(intTime1 > 800){
+            fill(255, 0, 0);
+            text("9",445, 530);
+            textSize(25);
+        }else if(intTime1 > 700){
+            fill(255, 0, 0);
+            text("8",445, 530);
+            textSize(25);
+        }else if(intTime1 > 600){
+            fill(255, 0, 0);
+            text("7",445, 530);
+            textSize(25);
+        }else if(intTime1 > 500){
+            fill(255, 0, 0);
+            text("6",445, 530);
+            textSize(25);
+        }else if(intTime1 > 400){
+            fill(255, 0, 0);
+            text("5",445, 530);
+            textSize(25);
+        }else if(intTime1 > 300){
+            fill(255, 0, 0);
+            text("4",445, 530);
+            textSize(25);
+        }else if(intTime1 > 200){
+            fill(255, 0, 0);
+            text("3",445, 530);
+            textSize(25);
+        }else if(intTime1 > 100){
+            fill(255, 0, 0);
+            text("2",445, 530);
+            textSize(25);
+        }else if(intTime1 > 0){
+            fill(255, 0, 0);
+            text("1",445, 530);
+            textSize(25);
+        }else{
+            fill(255, 0, 0);
+            text("0",445, 530);
+            textSize(25);
+        }
+    }
+
+
+    public void gameOver() {
+        // Game over logic
+        textAlign(CENTER, CENTER);
+        textSize(50);
+        fill(255, 0, 0);
+        text("Game Over",width/2, height/2);
+        noLoop();
+    }
+
+    public void gameWon() {
+        // Game won logic
+        textAlign(CENTER, CENTER);
+        textSize(50);
+        fill(0, 255, 0);
+        text("You Won!", width/2, height/2);
+        noLoop();
+    }
+
+
+    public void keyPressed() {
+        if (intCatcherX < 0){
+            intCatcherX = 0;
+        }
+
+        if (intCatcherX > 650){
+            intCatcherX = 650;
+        }
+        
+        // if LEFT is pressed, catcher moves to the left
+        if (keyCode == LEFT) {
+            intCatcherX -= 15;
+        }
+        // if RIGHT is pressed, catcher moves to the right
+        else if (keyCode == RIGHT) {
+            intCatcherX += 15;
+        }
+    }
+      
+      // define other methods down here.
+    
+    
+    
 }
+    
+   
--- Sketch1.java
+++ Sketch1.java
@@ -1,36 +1,178 @@
 import processing.core.PApplet;
+import processing.core.PImage;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class Sketch1 extends PApplet {
-	
-	
-  /**
-   * Called once at the beginning of execution, put your size all in this method
-   */
-  public void settings() {
-	// put your size call here
-    size(400, 400);
-  }
 
-  /** 
-   * Called once at the beginning of execution.  Add initial set up
-   * values here i.e background, stroke, fill etc.
-   */
-  public void setup() {
-    background(210, 255, 173);
-  }
+    PImage imgBackground;
+    PImage[] images = new PImage[4];  // Array to hold the four images
+    Float[] imageX = new Float[4];    // Array to hold the X positions of the images
+    Float[] imageY = new Float[4];    // Array to hold the Y positions of the images
+    int[] imageColors = new int[4];   // Array to hold the colors of the images
+    float speed = 5;                  // Speed at which the images fall
 
-  /**
-   * Called repeatedly, anything drawn to the screen goes here
-   */
-  public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    ArrayList<Integer> order = new ArrayList<>();  // ArrayList to hold the correct order of the images
+    ArrayList<Integer> caught = new ArrayList<>(); // ArrayList to hold the images caught by the catcher
+    int lives = 3;                                 // Number of lives
+    int catcherColor = color(255, 0, 0);  // Color of the catcher
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    float catcherX;  // X position of the catcher
+    float catcherSpeed = 10;  // Speed at which the catcher moves
+
+
+    public void settings() {
+      // size call
+      size(800, 600);
+    }
+
+    public void setup() {
+      imgBackground = loadImage("Kitchen.png");
+      imgBackground.resize(800, 600);
+
+       // Set the initial position of the catcher
+      catcherX = width / 2;
+
+      // Set the initial color of the catcher
+      catcherColor = color(255, 0, 0);  // Red
+        
+      Arrays.setAll(imageX, i -> 0f);
+      Arrays.setAll(imageY, i -> (float)(images[i].height) * -1);
+
+
+      // Load the four images into the array
+      for (int i = 0; i < 4; i++) {
+        images[i] = loadImage("data/image" + (i + 1) + ".jpg");  // Replace "image" with the actual name and extension of your image files
+        imageColors[i] = color(255);  // Set the default color of the images to white
+      }
+
+      // Set the colors of the images
+      imageColors[0] = color(255, 0, 0);   // Red
+      imageColors[1] = color(0, 255, 0);   // Green
+      imageColors[2] = color(0, 0, 255);   // Blue
+
+      // Shuffle the arrays of image positions and create the correct order
+      /*Arrays.fill(imageX, 0f);
+      Arrays.fill(imageY, -height);
+      */
+      order.addAll(Arrays.asList(0, 1, 2, 3));  // Add the indices of the images in the original order
+      Collections.shuffle(Arrays.asList(imageX));
+      Collections.shuffle(Arrays.asList(imageY));
+      Collections.shuffle(order);
+
+      // Set the initial position of the catcher
+      catcherX = width / 2;
+    }
+
+    public void draw() {
+      // background(255);  // Clear the background
+      // add background 
+      image(imgBackground, 0, 0);
+
+      // Update the positions of the images
+      for (int i = 0; i < imageY.length; i++) {
+        imageY[i] += speed;
+
+        // Reset the position of the image when it reaches the bottom of the screen
+        if (imageY[i] > height) {
+          imageY[i] = (float)(images[i].height) * -1;
+          imageX[i] = random(width);
+        }
+
+        // Check if the catcher caught the image
+        if (imageY[i] + images[i].height >= height) {
+          if (caught.size() < order.size() && order.get(caught.size()) == i && imageColors[i] == catcherColor) {
+            caught.add(i);
+          } else {
+            lives--;
+            if (lives == 0) {
+              gameOver();
+            }
+          }
+        }
+      }
   
-  // define other methods down here.
+      for (int i = 0; i < images.length; i++) {
+        // push();
+        strokeWeight(5);
+        stroke(imageColors[i]);
+        image(images[i], imageX[i], imageY[i]);
+        // pop();
+      }
+
+      // Draw the catcher with the selected color
+      drawCatcher();
+
+      // Draw the lives
+      drawLives();
+
+      // Check if the player won
+      if (caught.size() == order.size()) {
+        gameWon();
+      }
+    }
+
+    public void drawCatcher() {
+      // Draw the catcher at the bottom center of the screen with the selected color
+      float catcherWidth = 100;
+      float catcherHeight = 20;
+      float catcherY = height - catcherHeight;
+
+      fill(catcherColor);
+      rect(catcherX, catcherY, catcherWidth, catcherHeight);
+    }
+
+    public void drawLives() {
+      // Draw the remaining lives at the top left of the screen
+      String livesText = "Lives: " + lives;
+      float livesX = 10;
+      float livesY = 20;
+
+      fill(0);
+      textSize(20);
+      text(livesText, livesX, livesY);
+    }
+
+    public void gameOver() {
+      // Game over logic
+      textAlign(CENTER, CENTER);
+      textSize(50);
+      fill(255, 0, 0);
+      text("Game Over",width/2, height/2);
+      noLoop();
+    }
+
+    public void gameWon() {
+      // Game won logic
+      textAlign(CENTER, CENTER);
+      textSize(50);
+      fill(0, 255, 0);
+      text("You Won!", width/2, height/2);
+      noLoop();
+    }
+
+    public void keyPressed() {
+      // Move the catcher left or right when the corresponding arrow key is pressed
+      if (keyCode == LEFT) {
+        catcherX -= catcherSpeed;
+      } else if (keyCode == RIGHT) {
+        catcherX += catcherSpeed;
+      } else if (key == '1') {
+        catcherColor = color(255,0,0);  // red
+      } else if (key == '2') {
+        catcherColor = color(0,255,0);  // green
+      } else if (key == '3') {
+        catcherColor = color(0,0,255);  // blue
+      } 
+    }
+
+    public void keyReleased() {
+      // Stop the catcher from moving when the arrow keys are released
+      if (keyCode == LEFT || keyCode == RIGHT) {
+        catcherX = constrain(catcherX, 0, width);  // Constrain the catcher's X position within the screen bounds
+      }
+    }
+
 }
